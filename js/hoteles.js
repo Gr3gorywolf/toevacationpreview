be7bba297a83fe4fa36bc7da0023c0351f73df6d
Gregory Cabral De Los Santos <Blackarcanjus@gmail.com>
2019-02-17 00:25:25 -0400

    finished the about us page

--- js/hoteles.js
+++ js/hoteles.js
@@ -1,13 +1,14 @@
 function loaded(){
 var containerhotels=document.getElementById("containerhoteles");
-
+var hotelsdata="";
     for(let i=0;i<data.length;i++){
-
-        containerhotels.innerHTML+=card_normal(data[i].nombre,data[i].descripcion,data[i].precio,data[i].imagen);
+        hotelsdata+=card_normal(data[i].nombre,data[i].descripcion,data[i].precio,data[i].imagen);
 
 
     }
 
+
+    containerhotels.innerHTML=hotelsdata;
 
 
 }
